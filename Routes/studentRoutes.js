1f4a601aebbef6300d3d6154cec8da094dd777e4
leabeldad <157865847+leabeldad@users.noreply.github.com>
2024-02-19 01:32:27 +0800

    Update studentRoutes.js

--- Routes/studentRoutes.js
+++ Routes/studentRoutes.js
@@ -5,7 +5,7 @@ const authenticateToken = require('../middlewares/authenticateToken');
 
 router.post('/create', studentController.create);
 router.post('/login', studentController.login);
-roter.get('/getAllStudent', studentController.getAll);
+router.get('/getAllStudent', studentController.getAll);
 router.post('/logout', studentController.logout);
 router.post('/getPermit', authenticateToken, studentController.getPermit); 
 router.put('/update', studentController.update); 
