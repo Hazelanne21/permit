9e308963887a0b58d6926d89761607d2540793de
leabeldad <157865847+leabeldad@users.noreply.github.com>
2024-02-27 19:32:25 +0800

    Update studentRoutes.js

--- Routes/studentRoutes.js
+++ Routes/studentRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const studentController = require('../controllers/studentController');
 const authenticateToken = require('../middlewares/authenticateToken');
 
-router.post('/create', authenticateToken, studentController.create);
-router.post('/login', authenticateToken, studentController.login);
+router.post('/create', studentController.create);
+router.post('/login', studentController.login);
 router.get('/getAllStudent', authenticateToken, studentController.getAll);
 router.post('/logout', authenticateToken, studentController.logout);
 router.post('/getPermit', authenticateToken, authenticateToken, studentController.getPermit); 
