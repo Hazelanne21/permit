d0a28d12ac8a435da37a341834ed4e383564866a
Hazelanne21 <118337179+Hazelanne21@users.noreply.github.com>
2024-02-27 12:22:27 +0800

    commit

--- Routes/studentRoutes.js
+++ Routes/studentRoutes.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const studentController = require('../controllers/studentController');
 const authenticateToken = require('../middlewares/authenticateToken');
 
-router.post('/create', studentController.create);
-router.post('/login', studentController.login);
-router.get('/getAllStudent', studentController.getAll);
-router.post('/logout', studentController.logout);
-router.post('/getPermit', authenticateToken, studentController.getPermit); 
-router.get('/permits', studentController.getAllPermits);
-router.put('/update', studentController.update); 
-router.delete('/delete', studentController.delete);
+router.post('/create', authenticateToken, studentController.create);
+router.post('/login', authenticateToken, studentController.login);
+router.get('/getAllStudent', authenticateToken, studentController.getAll);
+router.post('/logout', authenticateToken, studentController.logout);
+router.post('/getPermit', authenticateToken, authenticateToken, studentController.getPermit); 
+router.get('/permits', authenticateToken, studentController.getAllPermits);
+router.put('/update', authenticateToken, studentController.update); 
+router.delete('/delete', authenticateToken, studentController.delete);
 
 
 module.exports = router;
